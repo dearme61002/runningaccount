c24bb8006e4b27b60be159d305a984622ee9a3ff
昶志 林 <dearme61002@gmail.com>
2021-08-03 13:54:32 +0800

    modols 與sqlhelp完成

---
+++ RunningAccount_7324/modols/AccountNote.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modols
+{
+    public class AccountNote
+    {
+        public int id { get; set; }
+        public string userid{get;set;}
+
+       public  string caption { get; set; }
+        public int amount { get; set; }
+        public int acttype { get; set; }
+        public DateTime createdate { get; set; }
+         
+        public string body { get; set; }
+    }
+}
---
+++ RunningAccount_7324/modols/UserInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modols
+{
+   public  class UserInfo
+    {
+        public string id { get; set; }
+        public string account { get; set; }
+        public string pwd { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public int userlevel { get; set; }
+        public DateTime createdate { get; set; }
+    }
+}
